863f077ef5f339e7dc6acee2b1c8c484c8e384f1
adyasha <adyashamohanty8104@gmail.com>
2020-05-16 16:39:38 +0530

    added the svg and fix the bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 window.onload = function () {
-    var fileupload = document.getElementById("pageUploader");
-    var button = document.getElementById("btnPageUpload");
-    button.onclick = function () {
-        fileupload.click();
+  var fileupload = document.getElementById("pageUploader");
+  var button = document.getElementById("btnPageUpload");
+  button.onclick = function () {
+    fileupload.click();
+  };
+  fileupload.onchange = function () {
+    console.log("page Uploader Triggered.");
+    var reader = new FileReader();
+    reader.readAsDataURL(fileupload.files[0]);
+    reader.onload = function (e) {
+      img = loadImage(e.target.result);
     };
-    fileupload.onchange = function () {
-        console.log("page Uploader Triggered.")
-        var reader = new FileReader();
-        reader.readAsDataURL(fileupload.files[0]);
-        reader.onload = function (e) {
-            img = loadImage(e.target.result);
-        }
-    };
+  };
 
-    var fontupload = document.getElementById("fontUploader");
-    button = document.getElementById("btnFontUpload");
-    button.onclick = function () {
-        fontupload.click();
+  var fontupload = document.getElementById("fontUploader");
+  button = document.getElementById("btnFontUpload");
+  button.onclick = function () {
+    fontupload.click();
+  };
+  fontupload.onchange = function () {
+    console.log("font Uploader Triggered.");
+    var reader = new FileReader();
+    reader.readAsDataURL(fontupload.files[0]);
+    reader.onload = function (e) {
+      myFont.push(loadFont(e.target.result));
+      myFonts += 1;
+      fontNum = myFonts - 1;
     };
-    fontupload.onchange = function () {
-        console.log("font Uploader Triggered.")
-        var reader = new FileReader();
-        reader.readAsDataURL(fontupload.files[0]);
-        reader.onload = function (e) {
-            myFont.push(loadFont(e.target.result))
-            myFonts += 1
-            fontNum = myFonts - 1
-        }
-    };
+  };
 };
