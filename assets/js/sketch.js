0444d15f0029813e354bcc69aa0e390481880aea
Anush Bhatia <40017559+anushbhatia@users.noreply.github.com>
2020-05-16 16:55:55 +0530

    Revert "UI changed and favicon added"

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,49 +1,49 @@
-myData = `The quick brown fox jumps over the lazy dog`;
-let img,
-  myFont = [];
-myFonts = 7;
-imgNum = 1;
-fontNum = 0;
-pageNum = 1;
-xaxis = 20;
-yaxis = 20;
-fontsize = 30;
-w = 700;
-linespacing = false;
+myData = `The quick brown fox jumps over the lazy dog`
+let img, 
+myFont = [];
+myFonts = 7
+imgNum = 1
+fontNum = 0
+pageNum = 1
+xaxis=20
+yaxis=20
+fontsize=30
+w=700
+linespacing=false
 function preload() {
-  fontLoad();
-  loadPage();
+    fontLoad();
+    loadPage();
 }
 
-function setup() {
-  canvas = createCanvas(750, 1000);
-  canvas.parent("contributing");
-  rectMode(CORNER);
+function setup(){
+    canvas = createCanvas(750,1000)
+    canvas.parent('contributing')
+    rectMode(CORNER);
 }
 
-function draw() {
-  image(img, 0, 0, width, height);
-  textFont(myFont[fontNum]);
-  textSize(fontsize);
-  fill("#264180");
-  if (linespacing) {
-    textLeading(linespacing);
-  }
-  data = "\n" + myData;
-  text(data, xaxis, yaxis, w, 900);
+function draw(){
+    image(img, 0, 0, width, height)
+    textFont(myFont[fontNum]);
+    textSize(fontsize)
+    fill('#264180')
+    if(linespacing){
+        textLeading(linespacing);
+    }
+    data = "\n"+myData
+    text(data, xaxis, yaxis, w, 900);
 }
 
-function fontLoad() {
-  for (var i = 0; i < myFonts; i++) {
-    myFont.push(loadFont("fonts/font (" + str(i) + ").ttf"));
-  }
+function fontLoad(){
+    for(var i = 0; i < myFonts; i++){
+        myFont.push(loadFont('fonts/font ('+str(i)+').ttf'));
+    }
 }
 
-function changeFont() {
-  fontNum += 1;
-  fontNum %= myFonts;
+function changeFont(){
+    fontNum += 1;
+    fontNum %= myFonts
 }
 
-function loadPage() {
-  img = loadImage("pages/page (2).jpg");
+function loadPage(){
+    img = loadImage('pages/page (2).jpg');
 }
