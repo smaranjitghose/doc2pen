d63a14b5714cca43cf3110f1981bf152d14e18fb
himanshujaidka <58654018+himanshujaidka@users.noreply.github.com>
2020-08-17 13:53:42 +0530

    character limit upto 1000

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,4 +53,14 @@ window.onload = function () {
                 }
             })();
 
-           
+            /*function for counting characters*/
+            function textCounter(field,field2,maxlimit)
+              {
+              var countfield = document.getElementById(field2);
+              if ( field.value.length > maxlimit ) {
+                field.value = field.value.substring( 0, maxlimit );
+                return false;
+              } else {
+                countfield.value = maxlimit - field.value.length;
+              }
+              }
