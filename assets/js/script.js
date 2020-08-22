5ca4101acd00a27a4e7915da37c1ec0f60f28b31
Ekjot Kaur <ekjotkaurk13@gmail.com>
2020-08-22 22:32:43 +0530

    Removed Dark Theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,29 +34,7 @@ window.onload = function () {
       fontNum = myFonts - 1;
     };
   };
-};// function to set a given theme/color-scheme
-function setTheme(themeName) {
-  localStorage.setItem("theme", themeName);
-  document.documentElement.className = themeName;
-}
-// function to toggle between light and dark theme
-function toggleTheme() {
-  if (localStorage.getItem("theme") === "theme-dark") {
-    setTheme("theme-light");
-    document.getElementById("switch").innerHTML = "Dark";
-  } else {
-    document.getElementById("switch").innerHTML = "Light";
-    setTheme("theme-dark");
-  }
-}
-// Immediately invoked function to set the theme on initial load
-(function () {
-  if (localStorage.getItem("theme") === "theme-dark") {
-    setTheme("theme-dark");
-  } else {
-    setTheme("theme-light");
-  }
-})();
+};
 
 //word count and limit function
 
