efc9c79074a659847cacce23eda271ca0ee6199c
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-08-24 23:17:00 +0530

    commit on Suggestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,21 @@ window.onload = function () {
       fontNum = myFonts - 1;
     };
   };
-
 };
+
+//word count and limit function
+
+$("#dataField").on("keydown", function (e) {// function event
+  var words = $.trim(this.value).length ? this.value.match(/\S+/g).length : 0; //count length of word when space occur
+  if (words <= 10000) {
+    $("#count-words").text(10000 - words);// subtracts word from 10K and targets span-id"count-words" 
+  } else {
+    if (e.which !== 8) e.preventDefault();//prevent user to enter more text
+  }
+});
+
+$(".dropdown-trigger").dropdown();
+
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
