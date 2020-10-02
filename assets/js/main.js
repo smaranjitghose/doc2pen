bf8683061f886b207eb89120d86b32eb9813aa64
Yash Maheshwari <yashmaheshwari551@gmail.com>
2020-10-02 13:12:23 +0530

    Updated the styling for the MenuBar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
 // Navigation Bar Declaration
 let header = $(`
-<nav class="navbar navbar-expand-lg navbar-light nav1" style="z-index: 9999" id="navbar">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top nav1" style="z-index: 9999" id="navbar">
   <a class="navbar-brand brand-logo" href="#"><img src="./assets/images/logo.png"></a>
   <button class="navbar-toggler res-toggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <b><a class="nav-link text-white menu-one" href="index.html">Home</a></b>
+        <b><a class="nav-link text-white px-3" href="index.html">Home</a></b>
       </li>
       <li class="nav-item active">
-        <b><a class="nav-link text-white" href="index.html#about-intro">About</a></b>
+        <b><a class="nav-link text-white px-3" href="index.html#about-intro">About</a></b>
       </li>
       <li class="nav-item active">
-        <b><a class="nav-link text-white" href="index.html#team">Team</a></b>
+        <b><a class="nav-link text-white px-3" href="index.html#team">Team</a></b>
       </li>
       <li class="nav-item active">
-        <b><a class="nav-link text-white" href="index.html#contact">Contact</a></b>
+        <b><a class="nav-link text-white px-3" href="index.html#contact">Contact</a></b>
       </li>
       <li class="nav-item active">
-        <b><a class="nav-link text-white" href="editor.html">Editor</a></b>
+        <b><a class="nav-link text-white px-3" href="editor.html">Editor</a></b>
       </li>
     </ul>
   </div>
@@ -40,21 +40,6 @@ bodyElement.append(footer);
 $(document).ready(function () {
   $(".sidenav").sidenav();
 });
-
-//sticky navbar
-
-window.onscroll = function () {
-  myFunction();
-};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 // Map added
 function initMap() {
