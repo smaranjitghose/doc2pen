967a16999ccbad95bdff296c1895b3a3be6a33c0
himanshujaidka <58654018+himanshujaidka@users.noreply.github.com>
2020-10-03 13:34:01 +0530

    need help

--- assets/js/gdrive.js
+++ assets/js/gdrive.js
@@ -1,94 +1,49 @@
-var CLIENT_ID = "570473460487-uobjo96uqc01mpdnegni48pf50k2rs6l.apps.googleusercontent.com";
-var API_KEY = "AIzaSyB2UzzbbXu_Wjht9tTSD5DuaaCvXbjHSmI"
+$(document).ready(function(){
+     
+    // client id of the project
 
- // Array of API discovery doc URLs for APIs used by the doc2pen
- var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+    var clientId = "-329654474267-i316ci25nkjuspj1bs6lbd7n8avj1lqc.apps.googleusercontent.com";
 
-  // Authorization scopes required by the API; multiple scopes can be
-    // included, separated by spaces.
-      var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+    // redirect_uri of the project
 
-      var authorizeButton = document.getElementById("authorize_button");
-      var signoutButton = document.getElementById("signout_button");
+    var redirect_uri = "https://doc2pen.smaranjitghose.codes";
 
-      //On load, called to load the auth2 library and API client library.
-      function handleClientload(){
-          gapi.load("client:auth2",initClient);
-      }
+    // scope of the project
 
-      //Initializes the API client library and sets up sign-in state listeners.
-      function initClient(){
-          gapi.client.init({
-              apiKey: API_KEY,
-              clientId: CLIENT_ID,
-              discoveryDocs: DISCOVERY_DOCS,
-              scope: SCOPES
-          }).then(function(){
-               // Listen for sign-in state changes.
-               gapi.auth2.getAuthInstance().isSignedIn().listen(updateSigninStatus);
+    var scope = "https://www.googleapis.com/auth/drive";
 
-               //handle the initial sign in state
-               updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-               authorizeButton.onclick = handleAuthClick;
-               signoutButton.onclick = handleSignoutClick;
-          }, function(error){
-              appendPRe(JSON.stringify(error,null,2));
+    // the url to which the user is redirected to
 
-        });
-      }
+    var url = "";
 
-      //Called when the signed in status changes, to update the UI appropriately. After a sign-in, the API is called.
-      function updateSigninStatus(isSignedIn){
-          if (isSignedIn){
-              authorizeButton.style.display = "none";
-              signoutButton.style.display = "block";
-              listFiles();
-          }else{
-              authorizeButton.style.display = "block";
-              signoutButton.style.display = "none";
-          }
-          
-      }
 
-      //Sign in the user upon button click.
-      function handleAuthClick(event){
-          gapi.auth2.getAuthInstance().signIn();
+    // this is event click listener for the button
 
-      }
+    $("#login").click(function(){
 
-      //Sign out the user upon button click.
-      function handleSignoutClick(event){
-          gapi.auth2.getAuthInstance().signOut();
-      }
+       // this is the method which will be invoked it takes four parameters
 
-        /**
-       * Append a pre element to the body containing the given message
-       * as its text node. Used to display the results of the API call.
-       */
-       //@param {string} message Text to be placed in pre element.
+       signIn(clientId,redirect_uri,scope,url);
 
-       function appendPre(message){
-           var pre = document.getElementById("content");
-           var textContent = document.createTextNode(message + "\n");
-           pre.appendChild(textContent);
-       }
+    });
 
-       //print files
-       function listFiles(){
-           gapi.client.drive.files.list({
-               "pageSize": 10,
-               "fields": "nextPageToken, files(id,name)"
-           }).then(function(response){
-               appendPre("Files:");
-               var files = response.result.files;
-               if(files && files.length>0){
-                   for (var i=0;i<files.length;i++){
-                       var file = files[i];
-                       appendPre(file.name+"("+file.id+")");
-                   }
-               }else{
-                   appendPre("No Files Found.");
-               }
-           });
-       }
-      
+    function signIn(clientId,redirect_uri,scope,url){
+     
+       // the actual url to which the user is redirected to 
+
+       url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+       +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+       +"&access_type=offline";
+
+       // this line makes the user redirected to the url
+
+       window.location = url;
+
+
+
+
+    }
+
+
+
+});
