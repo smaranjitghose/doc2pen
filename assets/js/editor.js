e70d04dac5c27712ba229129b3a00a5c539e7dc8
Anu Sharma <sharmaanu2002in@gmail.com>
2021-01-04 23:13:26 +0530

    Added the feature for multi-page document

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -134,3 +134,35 @@ $("#dataField").on("keydown", function (e) {// function event
     if (e.which !== 8) e.preventDefault();//prevent user to enter more text
   }
 });
+function getPDF(){
+
+  var HTML_Width = $(".container-input").width();
+  var HTML_Height = $(".container-input").height();
+  var top_left_margin = 15;
+  var PDF_Width = HTML_Width+(top_left_margin*2);
+  var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+  var canvas_image_width = HTML_Width;
+  var canvas_image_height = HTML_Height;
+  
+  var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+  
+
+  html2canvas($(".container-input")[0],{allowTaint:true}).then(function(canvas) {
+    canvas.getContext('2d');
+    
+    console.log(canvas.height+"  "+canvas.width);
+    
+    
+    var imgData = canvas.toDataURL("image/jpeg", 1.0);
+    var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+      pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+    
+    
+    for (var i = 1; i <= totalPDFPages; i++) { 
+      pdf.addPage(PDF_Width, PDF_Height);
+      pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+    }
+    
+      pdf.save("HTML-Document.pdf");
+      });
+};
