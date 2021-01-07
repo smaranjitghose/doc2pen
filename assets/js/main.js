59e2427bf5ead730eb858fcbc43b04f33bef760b
Bhoomika Valani <bhoomikavalani07@gmail.com>
2021-01-07 17:23:59 +0530

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,8 +26,8 @@ let header = $(`
         <b><a class="nav-link" href="editor.html">Editor</a></b>
       </li>
       <li class="nav-item active">
-        <b><a class="nav-link" href="painter.html">Paint</a></b>
-      </li>
+      <b><a class="nav-link" href="painter.html">Paint</a></b>
+    </li>
     </ul>
   </div>
 </nav>`);
