f08e4571a9e9bc7638044945635d635d0e47f060
Anush Bhatia <anushbhatia1234@gmail.com>
2021-01-09 15:47:37 +0530

    Base Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./index.css";
+import "./App.css";
+import MainContainer from "./components/MainContainer/MainContainer";
+import Footer from "./components/Footer/Footer";
+import EditContextProvider from "./containers/editContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div>
+          <a
+            href="https://dscommunity.in"
+            target="_blank"
+            rel="noreferrer noopener"
+            style={{
+              position: "fixed",
+              zIndex: "1000",
+              right: "-1rem",
+              top: "1rem",
+            }}
+            className="col-2 col-md-1 d-none d-lg-block"
+          >
+          </a>
+          <EditContextProvider>
+            <div id="app" style={{ height: "100%" }}>
+              <MainContainer />
+            </div>
+          </EditContextProvider>
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,9 @@
+import style from "./Slider.css";
+
+const Sliders = () => (
+  <div class={style.slidderBtn}>
+    <div class="sliderBarcontainer">{""}</div>
+  </div>
+);
+
+export default Sliders;
