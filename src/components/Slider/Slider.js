ba732e7e5db4f0087f184ed17f8e30beecd7c20a
Anush Bhatia <anushbhatia1234@gmail.com>
2021-01-09 14:45:14 +0530

    Hello

--- src/components/Slider/Slider.js
+++
@@ -1,9 +0,0 @@
-import style from "./Slider.css";
-
-const Sliders = () => (
-  <div class={style.slidderBtn}>
-    <div class="sliderBarcontainer">{""}</div>
-  </div>
-);
-
-export default Sliders;
