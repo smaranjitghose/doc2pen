a74b21d6df5321fe9fce2483939b7b46e94491d6
Neetu Agarwal <agarwalneetu301@gmail.com>
2021-01-10 22:08:59 +0530

    requested changes made

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
-// import emoji from "react-easy-emoji";
+import emoji from "react-easy-emoji";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
   return (
+    //py-2 for padding
     <div className="footer-div py-2">
       <Fade>
         <p className="footer-text">
-        {"Made with ❤️ in India for the students of the world."} 
+        {emoji("Made with ❤️ in India for the students of the world.")} 
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
