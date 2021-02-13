dba2128defcd45cfbea1554e9208f178fb718d61
Amit kumar mishra <akm43300@gmail.com>
2021-02-13 18:17:00 +0530

    Tailwind CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import styles from './Header.module.css';
 import logo from './logo.png';
 import {ImInfo} from "react-icons/im";
 import {RiTeamLine, RiContactsBook2Line} from "react-icons/ri";
-import {BiDonateHeart, BiMenu} from "react-icons/bi";
+import {BiDonateHeart} from "react-icons/bi";
 
 const quickLinks = [
     {
@@ -45,23 +45,32 @@ function Header() {
     return (
         <>
             <header className={styles.Header}>
+              <div className="container mx-auto">
+               <div className="sm:flex justify-around">
                 <img className={styles.left} src={logo} alt="Logo"/>
-                <div className={styles.right}>
-                    <Link to='home_banner' offset={-15}>
+                  <ul className="text-gray-700 sm:self-center text-xl border-t sm:border-none">
+                    <li className="sm:inline-block">
+                    <Link to='home_banner' >
                         <NavLink className={styles.header_links} to='/' exact activeClassName={styles.header_active_links}>
                             Home
                         </NavLink>
                     </Link>
+                    </li>
+                    <li className="sm:inline-block">
                     <NavLink className={styles.header_links} to='/editor' exact activeClassName={styles.header_active_links}>
                         Editor
                     </NavLink>
+                    </li>
+                    <li className="sm:inline-block">
                     <NavLink className={styles.header_links} to='/sketch' exact activeClassName={styles.header_active_links}>
                         Sketch <sup><span style={{color: "red", fontSize: "0.7rem"}}>New</span></sup>
                     </NavLink>
+                    </li>
+                    </ul>
+                
+               
                 </div>
-                <div className={styles.hamburger}>
-                    <BiMenu size={30}/>
-                </div>
+              </div>
             </header>
             <div className={styles.quick_box}>
                 {
