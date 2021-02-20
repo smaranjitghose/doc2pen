10493932dbb802259f1c407a52d9a006495bd29a
ashuvssut <ashu.khanduala@gmail.com>
2021-02-20 23:32:42 +0530

    [feature]: Select on text areas to toggle settings mode ( body or title)

--- src/pages/Editor/sections/Settings/Settings.js
+++ src/pages/Editor/sections/Settings/Settings.js
@@ -16,11 +16,11 @@ const Settings = () => {
             type="checkbox"
             name="heading"
             value={editContext.isBody}
-            id="heading"
+            id="title-heading"
             onClick={editContext.isBodyHandler}
-            className={`d-none`}
+            className={`id-title d-none`}
           />
-          <label for="heading" style={{ color: editContext.isBody && "#f0f7ff" }}>
+          <label for="title-heading" style={{ color: editContext.isBody && "#f0f7ff" }}>
             Title
           </label>
         </div>
@@ -29,11 +29,11 @@ const Settings = () => {
             type="checkbox"
             name="heading"
             value={editContext.isBody}
-            id="heading"
+            id="body-heading"
             onClick={editContext.isBodyHandler}
-            className={`d-none`}
+            className={`id-body d-none `}
           />
-          <label for="heading" style={{ color: !editContext.isBody && "#f0f7ff" }}>
+          <label for="body-heading" style={{ color: !editContext.isBody && "#f0f7ff" }}>
             Body
           </label>
         </div>
@@ -58,9 +58,11 @@ const Settings = () => {
           <Dropdown name="Change Sheet" type="page" items={["Ruled1", "Ruled2", "OnlyMargin", "Blank1", "Blank2"]} />
 
           <div className={styles.vSeparator}></div>
-          
-          <label className={styles.downloadBtn} htmlFor="import">Import File</label>
-          <input id="import" style={{display:"none"}} type="file" onChange={editContext.importTxt}></input>
+
+          <label className={styles.downloadBtn} htmlFor="import">
+            Import File
+          </label>
+          <input id="import" style={{ display: "none" }} type="file" onChange={editContext.importTxt}></input>
           <div className={styles.vSeparator}></div>
 
           <button className={styles.downloadBtn} onClick={editContext.downloadImg}>
