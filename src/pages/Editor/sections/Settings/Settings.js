e279c88b5dd17e507cfa704f90e9461c8aa6d8ab
Mukta Sawant <muktasawant284@gmail.com>
2021-02-24 23:31:26 +0530

    init

--- src/pages/Editor/sections/Settings/Settings.js
+++ src/pages/Editor/sections/Settings/Settings.js
@@ -45,7 +45,7 @@ const Settings = () => {
           <Dropdown
             name="Change Style"
             type="font"
-            items={["HomemadeApple", "Caveat", "Dawning", "IndieFlower", "NothingYouCouldDo", "Liu", "LeagueScript", "Maria"]}
+            items={["HomemadeApple", "Caveat", "Dawning", "IndieFlower", "NothingYouCouldDo", "Liu", "LeagueScript", "Enola"]}
             item1="HomemadeApple"
             item2="Caveat"
             item3="Dawning"
@@ -53,7 +53,7 @@ const Settings = () => {
             item5="NothingYouCouldDo"
             item6="Liu"
             item7="LeagueScript"
-            item8="Maria"
+            item8="Enola"
           />
           <div className={styles.vSeparator}></div>
           <Dropdown name="Change Sheet" type="page" items={["Ruled1", "Ruled2", "OnlyMargin", "Blank1", "Blank2"]} />
