48e41f7ff4ef875722ea88539e1db7ef353d2414
Nijin-26 <nijinazarkpz@gmail.com>
2021-03-10 13:08:26 +0530

    3 More Paper Frontly Font Colors added to Editor Page

--- src/pages/Editor/sections/Settings/Settings.js
+++ src/pages/Editor/sections/Settings/Settings.js
@@ -7,11 +7,16 @@ import NumberSlider from "../../components/Slider/NumberSlider";
 
 const Settings = () => {
   const editContext = useContext(EditContext);
-  
+
   return (
     <div className={styles.controlPanel}>
       <div className={styles.selector}>
-        <div className={styles.selectOption} style={{ backgroundColor: editContext.isBody ? "#1979CA" : "#f0f7ff" }}>
+        <div
+          className={styles.selectOption}
+          style={{
+            backgroundColor: editContext.isBody ? "#1979CA" : "#f0f7ff",
+          }}
+        >
           <input
             type="checkbox"
             name="heading"
@@ -20,11 +25,19 @@ const Settings = () => {
             onClick={editContext.isBodyHandler}
             className={`id-title d-none`}
           />
-          <label for="title-heading" style={{ color: editContext.isBody && "#f0f7ff" }}>
+          <label
+            for="title-heading"
+            style={{ color: editContext.isBody && "#f0f7ff" }}
+          >
             Title
           </label>
         </div>
-        <div className={styles.selectOption} style={{ backgroundColor: editContext.isBody ? "#f0f7ff" : "#1979CA" }}>
+        <div
+          className={styles.selectOption}
+          style={{
+            backgroundColor: editContext.isBody ? "#f0f7ff" : "#1979CA",
+          }}
+        >
           <input
             type="checkbox"
             name="heading"
@@ -33,29 +46,62 @@ const Settings = () => {
             onClick={editContext.isBodyHandler}
             className={`id-body d-none `}
           />
-          <label for="body-heading" style={{ color: !editContext.isBody && "#f0f7ff" }}>
+          <label
+            for="body-heading"
+            style={{ color: !editContext.isBody && "#f0f7ff" }}
+          >
             Body
           </label>
         </div>
       </div>
       <div className={styles.controls}>
         <div className={styles.group1}>
-          <Dropdown name="Change Color" type="color" items={["black", "red", "blue", "green", "pink"]} />
+          <Dropdown
+            name="Change Color"
+            type="color"
+            items={[
+              "black",
+              "red",
+              "orange",
+              "blue",
+              "green",
+              "pink",
+              "violet",
+              "dodgerblue",
+            ]}
+          />
           <div className={styles.vSeparator}></div>
           <Dropdown
             name="Change Style"
             type="font"
-            items={["HomemadeApple", "Caveat", "Dawning", "IndieFlower", "NothingYouCouldDo", "Liu", "LeagueScript"]}
+            items={[
+              "HomemadeApple",
+              "Caveat",
+              "Dawning",
+              "IndieFlower",
+              "NothingYouCouldDo",
+              "Liu",
+              "LeagueScript",
+            ]}
           />
           <div className={styles.vSeparator}></div>
-          <Dropdown name="Change Sheet" type="page" items={["Ruled1", "Ruled2", "OnlyMargin", "Blank1", "Blank2"]} />
+          <Dropdown
+            name="Change Sheet"
+            type="page"
+            items={["Ruled1", "Ruled2", "OnlyMargin", "Blank1", "Blank2"]}
+          />
 
           <div className={styles.vSeparator}></div>
 
           <label className={styles.downloadBtn} htmlFor="import">
             Import File
           </label>
-          <input id="import" style={{ display: "none" }} type="file" onChange={editContext.importTxt}></input>
+          <input
+            id="import"
+            style={{ display: "none" }}
+            type="file"
+            onChange={editContext.importTxt}
+          ></input>
           <div className={styles.vSeparator}></div>
 
           <Dropdown
