a0ea5fd0104e897cec58de258c2f06c9d00fbc7b
123mansha <manshasrivastava7@gmail.com>
2021-03-16 13:38:26 +0530

    modified the experience form

--- src/pages/Contact/components/EmojiRating/EmojiRating.jsx
+++ src/pages/Contact/components/EmojiRating/EmojiRating.jsx
@@ -4,7 +4,7 @@ import styles from "./EmojiRating.module.css";
 function EmojiRating() {
   return (
     <div className={styles.emojis}>
-      <label>
+      {/*<label>
         <input className={styles.radio} type="radio" value="1" name="feedback" />
         <span className={styles.emoji}>&#128553;</span>
       </label>
@@ -28,8 +28,33 @@ function EmojiRating() {
         <input className={styles.radio} type="radio" value="5" name="feedback" />
         <span className={styles.emoji}>&#128516;</span>
       </label>
+           
+      */}
+      <label>
+        <input className={styles.radio} type="radio" value="1" name="feedback" />
+        <span className={styles.emoji}>&#128077;</span>
+      </label>
+
+      <label>
+        <input className={styles.radio} type="radio" value="2" name="feedback" />
+        <span className={styles.emoji}>&#128078;</span>
+      </label>
+
+      <label>
+        <input className={styles.radio} type="radio" value="3" name="feedback" />
+        <span className={styles.emoji}>&#128079;</span>
+      </label>
+
+      <label>
+        <input className={styles.radio} type="radio" value="4" name="feedback" />
+        <span className={styles.emoji}>&#128147;</span>
+      </label>
+
+      <label>
+        <input className={styles.radio} type="radio" value="5" name="feedback" />
+        <span className={styles.emoji}>&#129327;</span>
+      </label>
     </div>
   );
 }
-
-export default EmojiRating
+export default EmojiRating 
