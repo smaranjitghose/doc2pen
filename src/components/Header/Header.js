6560a3f0231c7aa5e20fd3f70b9bcd464c1ad892
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-18 09:45:54 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import React,{useState, useEffect, useRef} from 'react';
-import {NavLink, useHistory} from 'react-router-dom';
-import {Link} from 'react-scroll';
+import React, { useState, useEffect, useRef } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import { Link } from 'react-scroll';
 import styles from './Header.module.css';
 import logo from './../../assets/logo/logo.webp';
-import {ImInfo} from "react-icons/im";
-import {RiTeamLine} from "react-icons/ri";
-import {BiDonateHeart, BiMenu} from "react-icons/bi";
+import { ImInfo } from "react-icons/im";
+import { RiTeamLine } from "react-icons/ri";
+import { BiDonateHeart} from "react-icons/bi";
 
 const quickLinks = [
     {
@@ -28,6 +28,9 @@ const quickLinks = [
 function Header() {
     let navLinkRef = useRef(null);
     let headerRef = useRef(null);
+    let btn1Ref = useRef(null);
+    let btn2Ref = useRef(null);
+    let btn3Ref = useRef(null);
     const history = useHistory();
 
     const [location, setLocation] = useState(window.location.pathname);
@@ -37,17 +40,22 @@ function Header() {
             setLocation(location.pathname);
         })
     }, [history])
-    
+
     function drop() {
+
         navLinkRef.current.classList.toggle(styles.rightDrop);
         headerRef.current.classList.toggle(styles.HeaderOpen);
+        btn1Ref.current.classList.toggle(styles.btn1);
+        btn2Ref.current.classList.toggle(styles.btn2);
+        btn3Ref.current.classList.toggle(styles.btn3);
+
     }
 
     return (
         <>
             <header ref={headerRef} className={`${styles.Header} ${styles.HeaderDrop}`}>
-                <NavLink  to='/'  >
-                    <img className={styles.left} src={logo} alt="Logo"/>
+                <NavLink to='/'  >
+                    <img className={styles.left} src={logo} alt="Logo" />
                 </NavLink>
                 <div ref={navLinkRef} className={`${styles.right} ${styles.rightDrop}`}>
                     <Link to='home_banner' offset={-15}>
@@ -65,8 +73,12 @@ function Header() {
                         Contact
                     </NavLink>
                 </div>
-                <div className={styles.hamburger} onClick={()=>drop()}>
-                    <BiMenu size={30}/>
+                <div className={styles.hamburger} onClick={() => drop()}>
+                    <div className={styles.menu_btn}>
+                        <div ref={btn1Ref} className={styles.btn_line}></div>
+                        <div ref={btn2Ref} className={styles.btn_line}></div>
+                        <div ref={btn3Ref} className={styles.btn_line}></div>
+                    </div>
                 </div>
             </header>
             <div className={styles.quick_box}>
