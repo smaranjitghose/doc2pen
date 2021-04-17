c41d1724d52602ae13262c0b658cad053623c9c3
ashuvssut <ashu.khanduala@gmail.com>
2021-04-18 00:54:00 +0530

    new Project Maintainer UI @home

--- src/pages/Home/sections/Team/Team.jsx
+++ src/pages/Home/sections/Team/Team.jsx
@@ -1,95 +1,80 @@
-import React, {useState, useEffect} from 'react';
-import styles from './team.module.scss';
-import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
-import smaranjitLarge from '../../../../assets/images/team/smaranjit_ghose_large.webp';
-import anushLarge from '../../../../assets/images/team/anush_bhatia_large.webp';
-import smaranjitSmall from "../../../../assets/images/team/smaranjit_ghose_small.webp";
-import anushSmall from "../../../../assets/images/team/anush_bhatia_small.webp";
-
+import React, { useState, useEffect } from "react";
+import styles from "./team.module.scss";
+import { FaGithub } from "react-icons/fa";
+import smaranjit from "../../../../assets/images/team/smaranjit_ghose_large.png";
+import anush from "../../../../assets/images/team/anush0.png";
+import anush0 from "../../../../assets/images/team/anush2.png";
+import anush1 from "../../../../assets/images/team/anush3.png";
+import FluidCard from "./components/FluidCard/FluidCard";
 
 function Team() {
+  const maintainerSection = [
+    {
+      name: "Smaranjit Ghose",
+      title: "Lead Developer",
+      githubLink: "https://github.com/smaranjitghose",
+      linkedinLink: "https://www.linkedin.com/in/smaranjitghose/",
+      imgArray: [smaranjit],
+    },
+    {
+      name: "Anush Bhatia",
+      title: "Lead Developer",
+      githubLink: "https://github.com/anushbhatia",
+      linkedinLink: "https://www.linkedin.com/in/anushbhatia/",
+      imgArray: [anush, anush0, anush1],
+    },
+  ];
 
-    const maintainerSection = [
-        {
-          imageLarge: smaranjitLarge,
-          imageSmall: smaranjitSmall,
-          name: "Smaranjit Ghose",
-          title: "Lead Developer",
-          github: "https://github.com/smaranjitghose",
-          linkedin: "https://www.linkedin.com/in/smaranjitghose/",
-          imgclass: "image_1"
-        },
-        {
-          imageLarge: anushLarge,
-          imageSmall: anushSmall,
-          name: "Anush Bhatia",
-          title: "Lead Developer",
-          github: "https://github.com/anushbhatia",
-          linkedin: "https://www.linkedin.com/in/anushbhatia/",
-          imgclass: "image_2"
-        }
-      ];
+  const [contributors, setContributors] = useState([]);
 
-      const [contributors, setContributors] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/repos/smaranjitghose/doc2pen/contributors?per_page=1000")
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        setContributors(
+          data.filter(element => !(element.login === "smaranjitghose" || element.login === "anushbhatia"))
+        );
+      });
+  }, []);
 
-      useEffect(() => {
-        fetch('https://api.github.com/repos/smaranjitghose/doc2pen/contributors?per_page=1000')
-        .then(res => res.json())
-        .then(data => {
-          // console.log(data);
-          setContributors(data.filter((element) => 
-            !(element.login === "smaranjitghose" || element.login === "anushbhatia")
-          ));
-        })
-      }, []);
-
-    return (
-        <div className={styles.Team} id="home_team">
-            {/* <!-- Project Maintainer Sub Section --> */}
-            <br/><br/> <p className={styles.titleMain}>Project Maintainers</p><br/><br/>
-            <div className={styles.projectMaintainers}>
-                {
-                    maintainerSection.map(item =>
-                      (
-                      <div className={`${styles[item.imgclass]} ${styles.centeralign}`} key={item.name}>
-                      <div className={styles.profile_container}>
-                        <div className={styles.profile_wrapper}>
-                          <div className={styles.profile_card}>
-                          <img srcSet={`${item.imageSmall} 1x, ${item.imageLarge} 2x`} src={item.imageSmall} alt="profile pics"/> 
-                          <h4>{item.name}</h4>
-                          <h5>{item.title}</h5>
-                          <div className={styles.icons}>
-                            <a href={item.github} target="_blank" rel="noreferrer"><FaGithub /></a>
-                            <a href={item.linkedin} target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
-                          </div> 
-                          </div>
-                        </div>
-                      </div>
-                      </div>))
-                }
-            </div>
-            
-            {/* <!-- Contributora Sub Section --> */}
-            <br/><br/>
-            <p className={styles.titleMain}>Clan of Contributors</p><br/><br/>
-            <div className={styles.team_container}>
-                {
-                  contributors.map(c => (
-                    <figure className={`${styles.position_relative} ${styles.d_inline_block} ${styles.text_center} ${styles.grid_wd_100} ${styles.figure}`} key={c.login}>
-                      <img src={c.avatar_url} className={styles.grid_wd_100} alt="Profile"/>
-                      <figcaption className={`${styles.position_absolute} ${styles.grid_wd_100} ${styles.va_top}`}>
-                        <div className={styles.text}>
-                          <a href={c.html_url} className={styles.social_icon} target="_blank" rel="noreferrer">
-                            <FaGithub />
-                          </a>
-                        </div>
-                      </figcaption>
-                    </figure>
-                  ))
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.Team} id="home_team">
+      {/* <!-- Project Maintainer Sub Section --> */}
+      <br />
+      <br /> <p className={styles.titleMain}>Project Maintainers</p>
+      <br />
+      <br />
+      <div className={styles.projectMaintainers}>
+        {maintainerSection.map((item, index) => (
+          <FluidCard content={item} key={index}/>
+        ))}
+      </div>
+      {/* <!-- Contributora Sub Section --> */}
+      <br />
+      <br />
+      <p className={styles.titleMain}>Clan of Contributors</p>
+      <br />
+      <br />
+      <div className={styles.team_container}>
+        {contributors.map(c => (
+          <figure
+            className={`${styles.position_relative} ${styles.d_inline_block} ${styles.text_center} ${styles.grid_wd_100} ${styles.figure}`}
+            key={c.login}
+          >
+            <img src={c.avatar_url} className={styles.grid_wd_100} alt="Profile" />
+            <figcaption className={`${styles.position_absolute} ${styles.grid_wd_100} ${styles.va_top}`}>
+              <div className={styles.text}>
+                <a href={c.html_url} className={styles.social_icon} target="_blank" rel="noreferrer">
+                  <FaGithub />
+                </a>
+              </div>
+            </figcaption>
+          </figure>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Team
+export default Team;
