6ff63a13178e4b1fd549991ad3df350f9bb3542a
ashuvssut <ashu.khanduala@gmail.com>
2021-05-07 11:35:12 +0530

    iconlibrary bugfix

--- src/pages/Sketch/components/IconLibrary/IconsLibrary.jsx
+++ src/pages/Sketch/components/IconLibrary/IconsLibrary.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import style from "./icon-lib.module.scss";
 import { Divider } from "@material-ui/core";
 
 // Put you icon pack import here
-const iconPack1SVGs = importAll(require.context("./icon-pack1", false, /\.svg$/));
-const iconPack21SVGs = importAll(require.context("./icon-pack2.1", false, /\.svg$/));
-const iconPack22SVGs = importAll(require.context("./icon-pack2.2", false, /\.svg$/));
-const iconPack3SVGs = importAll(require.context("./icon-pack3", false, /\.svg$/));
-const iconPack41SVGs = importAll(require.context("./icon-pack4.1", false, /\.svg$/));
-const iconPack42SVGs = importAll(require.context("./icon-pack4.2", false, /\.svg$/));
+const iconPack1SVGs = importAll(
+  require.context("./icon-pack1", false, /\.svg$/)
+);
+const iconPack21SVGs = importAll(
+  require.context("./icon-pack2.1", false, /\.svg$/)
+);
+const iconPack22SVGs = importAll(
+  require.context("./icon-pack2.2", false, /\.svg$/)
+);
+const iconPack3SVGs = importAll(
+  require.context("./icon-pack3", false, /\.svg$/)
+);
+const iconPack41SVGs = importAll(
+  require.context("./icon-pack4.1", false, /\.svg$/)
+);
+const iconPack42SVGs = importAll(
+  require.context("./icon-pack4.2", false, /\.svg$/)
+);
 
 function importAll(r) {
   let images = {};
@@ -23,21 +35,24 @@ function IconPreview(props) {
 
   const allSVGsRef = useRef([]);
 
-  useEffect(() => {
-    allSVGsRef.current = Object.keys(iconPackSVGs).map(imgName => {
-      return (
-        <label htmlFor={`icon-${imgName}`} title={imgName}>
-          <img id={`icon-${imgName}`} className={style.svgIcon} src={iconPackSVGs[imgName]} alt="" />
-        </label>
-      );
-    });
-  }, [iconPackSVGs]);
+  allSVGsRef.current = Object.keys(iconPackSVGs).map(imgName => {
+    return (
+      <label htmlFor={`icon-${imgName}`} title={imgName}>
+        <img
+          id={`icon-${imgName}`}
+          className={style.svgIcon}
+          src={iconPackSVGs[imgName]}
+          alt=""
+        />
+      </label>
+    );
+  });
 
   return (
     <div className={style.iconCategory}>
       <h1 className={style.categoryHeading}>{categoryTitle}</h1>
       <div className={style.svgContainer}>{allSVGsRef.current}</div>
-      <Divider style={{margin: "40px 0"}}/>
+      <Divider style={{ margin: "40px 0" }} />
     </div>
   );
 }
@@ -47,19 +62,37 @@ function IconsLibrary(props) {
     <div className={style.root}>
       <div className={style.panel}>{/* search bar */}</div>
       <div className={style.iconLib}>
-        <IconPreview iconPackSVGs={iconPack1SVGs} categoryTitle="Tech Stack Icons" />
+        <IconPreview
+          iconPackSVGs={iconPack1SVGs}
+          categoryTitle="Tech Stack Icons"
+        />
         {/* https://drwn.io/ */}
-        <IconPreview iconPackSVGs={iconPack21SVGs} categoryTitle="Stick Figure Icons - Light" />
-        <IconPreview iconPackSVGs={iconPack22SVGs} categoryTitle="Stick Figure Icons - Bold" />
+        <IconPreview
+          iconPackSVGs={iconPack21SVGs}
+          categoryTitle="Stick Figure Icons - Light"
+        />
+        <IconPreview
+          iconPackSVGs={iconPack22SVGs}
+          categoryTitle="Stick Figure Icons - Bold"
+        />
         {/* https://svgsilh.com/tag/stickman-1.html */}
-        <IconPreview iconPackSVGs={iconPack3SVGs} categoryTitle="Speech Bubble Icons" />
+        <IconPreview
+          iconPackSVGs={iconPack3SVGs}
+          categoryTitle="Speech Bubble Icons"
+        />
         {/* https://drwn.io/ */}
         {/* https://freesvg.org/search/ */}
         {/* https://www.flaticon.com/free-icons/hand-drawn-speech-bubble */}
         {/* https://www.flaticon.com/packs/speech-bubbles-2 */}
         {/* https://www.svgrepo.com/svg/82688/thought-bubble */}
-        <IconPreview iconPackSVGs={iconPack41SVGs} categoryTitle="Devices & Hardware Icons - Bold" />
-        <IconPreview iconPackSVGs={iconPack42SVGs} categoryTitle="Devices & Hardware Icons - Light" />
+        <IconPreview
+          iconPackSVGs={iconPack41SVGs}
+          categoryTitle="Devices & Hardware Icons - Bold"
+        />
+        <IconPreview
+          iconPackSVGs={iconPack42SVGs}
+          categoryTitle="Devices & Hardware Icons - Light"
+        />
         {/* https://www.svgrepo.com/vectors/device/ */}
         {/* https://www.flaticon.com/packs/smart-devices?k=1615927940770 */}
         {/* https://freeicons.io/material-icons-file-3/devices-icon-17364 */}
