b5dee0f409e7164cbf7a2c51a22a33c0886f7e6b
Nehal Sharma <68962290+N-Shar-ma@users.noreply.github.com>
2021-05-24 18:38:07 -0700

    Updated Pitch to include all functionalities

--- src/pages/Home/sections/About/About.jsx
+++ src/pages/Home/sections/About/About.jsx
@@ -27,12 +27,13 @@ function About() {
             <div className={styles.title}>
                 What are we about?
             </div>
-            <div className={styles.line}>One day, <b><i>Smitha</i></b> was very tensed about the assignments she has to submit in her college, because <b><i>She could Type Fast, But couldn't Write Fast enough.</i></b></div>
-            <div className={styles.line}>On talking about this issue with her friend <b><i>Harry</i></b>, he suggested <b><i>Doc2Pen</i></b> as a solution to her problem.</div>
-            <div className={styles.line}><b><i>Are you facing the same problem as Smitha did?</i></b></div>
-            
-            <div className={styles.line}>So, let's hear what Harry has to say about <b><i>Doc2Pen.</i></b></div>
-            <div className={styles.line}><b><i>Doc2Pen</i></b> converts your <b><i>typed assignments</i></b> into <b><i>handwritten</i></b> ones.</div>
+            <div className={styles.line}>Are you a <b><i>student</b></i> who is tired of having to <b><i>write out assignments on paper, draw sketches by hand, scan each page, convert it all to a PDF</b></i>, and finally submit?</div>
+            <div className={styles.line}>Does it seem <b><i>too much of a hassle</b></i> in this age of online education?</div>
+            <div className={styles.line}>If your answer is <b><i>yes</b></i>, then you've come to the <b><i>right place!</b></i></div>            
+            <div className={styles.line}><b><i>Doc2Pen</b></i> is the 1 stop shop for getting all your <b><i>"handmade" assignments ready for submission digitally</b></i>.</div>
+            <div className={styles.line}>Use the highly customisable <b><i>Editor page</b></i> to <b><i>type in text and get a handwritten document!</b></i></div>            
+            <div className={styles.line}>Use the <b><i>Sketch page</b></i> to <b><i>digitally draw in a handmade style</b></i>!</div> 
+            <div className={styles.line}>And in the end, use the <b><i>Media Manip page</b></i> to <b><i>convert your assignment into the appropriate format</b></i> for submission!</div> 
 
             <div className={styles.steps}>
                 <Step
