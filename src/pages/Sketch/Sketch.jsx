4162177572114909c5c55a9163db94194c908063
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-29 19:59:06 +0530

    eslint config added and errors corrected

--- src/pages/Sketch/Sketch.jsx
+++ src/pages/Sketch/Sketch.jsx
@@ -5,16 +5,16 @@ import Canvas from "./components/Canvas";
 import styles from "./sketch.module.scss";
 
 function Sketch() {
-  return (
-    <section className={styles.Sketch}>
-      <MetaComponent
-        title={metaData.sketch.title}
-        description={metaData.sketch.description}
-        keywords={metaData.sketch.keywords}
-      />
-      <Canvas />
-    </section>
-  );
+	return (
+		<section className={styles.Sketch}>
+			<MetaComponent
+				title={metaData.sketch.title}
+				description={metaData.sketch.description}
+				keywords={metaData.sketch.keywords}
+			/>
+			<Canvas />
+		</section>
+	);
 }
 
 export default Sketch;
