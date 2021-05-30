a8a0a74e77821855794c31da6db3c2f2c81df699
ashuvssut <ashu.khanduala@gmail.com>
2021-05-30 10:29:27 +0530

    new preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -4,7 +4,14 @@ import style from "./preloader.module.scss";
 function Preloader() {
   return (
     <div className={style.preloader}>
-      <div className={style.spinner}></div>
+      <span className={style.circle + " " + style.circle1}></span>
+      <span className={style.circle + " " + style.circle2}></span>
+      <span className={style.circle + " " + style.circle3}></span>
+      <span className={style.circle + " " + style.circle4}></span>
+      <span className={style.circle + " " + style.circle5}></span>
+      <span className={style.circle + " " + style.circle6}></span>
+      <span className={style.circle + " " + style.circle7}></span>
+      <span className={style.circle + " " + style.circle8}></span>
     </div>
   );
 }
