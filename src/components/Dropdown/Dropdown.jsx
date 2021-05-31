d8b87b2d5f1376b3b08e33cb9f86a4ed49cc65ff
ashuvssut <ashu.khanduala@gmail.com>
2021-06-01 02:11:25 +0530

    Remove package-lock.json and clean install using yarn only

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -53,9 +53,9 @@ const DropdownComponent = props => {
 							props.type === "download"
 								? aItem
 								: `body${
-										props.type === "font"
-											? "Font"
-											: props.type === "font-weight"
+									props.type === "font"
+										? "Font"
+										: props.type === "font-weight"
 											? "FontWeight"
 											: "Color"
 								  }`
